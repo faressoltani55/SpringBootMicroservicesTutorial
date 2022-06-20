5219d0ea10f4812d853f72c523d3a8acbd48a82d
Fares <soltani.fares.01@gmail.com>
2022-06-20 14:38:07 +0100

    setting up the first microservice and the docker postgresql containers

---
+++ customer/src/main/java/com/fares_tutorials/customer/CustomerService.java
@@ -0,0 +1,16 @@
+package com.fares_tutorials.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        // Check if email is valid and not taken
+        // store in db
+    }
+}
